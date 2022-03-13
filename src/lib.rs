7959b9e07fa94b4931bb7bdd52613cf8f57b029f
Philipp Ahlner <philipp@ahlner.com>
2022-03-13 11:36:55 +0100

    NodeMessageType implemented

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,19 @@
 //! [`Eclipse Tahu`]: https://github.com/eclipse/tahu/blob/master/sparkplug_b/sparkplug_b.proto
 //! [`Eclipse Sparkplug project`]: https://sparkplug.eclipse.org
 mod topic_namespace;
+pub use topic_namespace::*;
+
 mod device_message_types;
+pub use device_message_types::*;
+
+mod node_message_types;
+pub use node_message_types::*;
 
 pub use protobuf;
 
 include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
 pub use sparkplug_b::*;
-pub use topic_namespace::*;
-pub use device_message_types::*;
+
 
 #[cfg(test)]
 mod tests {
